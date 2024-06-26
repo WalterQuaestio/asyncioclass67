40a620ca15f16b59dd7ac3dd84774746c7d621ec
WalterQuaestio <thanagoonm16@gmail.com>
2024-06-26 12:22:28 +0700

    ส่งาน

--- assignment1/thread01.py
+++ assignment1/thread01.py
@@ -1,1 +1,19 @@
 # running a function in another thread
+from time import sleep,ctime
+from threading import Thread
+
+# a custom function that block for a moment
+def task(sleep_time,message):
+    #block for a moment 
+    sleep(1)
+    #display a massage
+    print(f'{ctime()} {message}')
+
+#create a thread
+thread = Thread(target =task,args=(1.5, 'New massage from another thread'))
+#run the thread
+thread.start()
+# wait for the htread to finish
+
+print(f'{ctime()}Waiting for the thread...')
+thread.join()
--- assignment2/simple01.py
+++ assignment2/simple01.py
@@ -1,2 +1,9 @@
 # Synchronous cooking
 # 1 kitchen 1 chefs 1 dish
+from time import sleep,ctime,time
+
+# Cooking synchronous
+def cooking(index):
+    print(f'{ctime()} Kitchen-{index}    : Begin cooking...')
+    sleep(2)
+    print(f'{ctime()} Kitchen-{index} :basket')
